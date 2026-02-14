bba1b1b177215944df2ba6887af25b41c133439c
SarabarSoft <contactus.sarabes@gmail.com>
2026-02-14 18:26:18 +0530

    firebase config updated

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,22 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service-account.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  // 👉 Vercel / production
+  serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  };
+} else {
+  // 👉 Local development
+  serviceAccount = require("../firebase-service-account.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
+
